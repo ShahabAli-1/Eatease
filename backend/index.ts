67423af01da86db6b584295ed0abbc0dbfdeec38
ShahabAli-1 <88381344+ShahabAli-1@users.noreply.github.com>
2024-08-02 07:16:26 +0500

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://eatease-ui.vercel.app",
     credentials: true,
   })
 );
